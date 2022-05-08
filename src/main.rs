3f44f2f29b875724a3d22a387e40ca65c8e9a1f5
George Campbell <campbell.goe@gmail.com>
2022-05-08 05:04:27 +0100

    rust guessing game initial commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guessing game. Guess the number.");
+
+    let secret_number = rand::thread_rng().gen_range(1..1001);
+
+    let mut num_guesses: u32 = 0;
+
+    loop {
+        let mut guess = String::new();
+
+        println!("Please input your guess (between 1 and 1000).");
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let guess = guess.trim();
+
+        if guess == "q" || guess == "quit" {
+            println!("Game was quit.");
+            break;
+        }
+
+        let guess: u32 = match guess.parse() {
+            Ok(num) => {
+                num_guesses += 1;
+                num
+            },
+            Err(_) => continue,
+        };
+
+        println!("Your guess was {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("Well done! Completed in {} guesses.", num_guesses);
+                break;
+            },
+            Ordering::Less => println!("Too small."),
+            Ordering::Greater => println!("Too big."),
+        }
+    }
+}
